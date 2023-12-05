3045f5fbfcf625bf9fc3b9a3087d717a1bd3f512
SantinyOficial <santiunido@gmail.com>
2023-12-04 20:01:19 -0500

    Actualizacion final

--- src/main/java/com/springboot/api/rest/controller/ClienteRestController.java
+++ src/main/java/com/springboot/api/rest/controller/ClienteRestController.java
@@ -26,11 +26,26 @@ public class ClienteRestController {
         return clienteService.findById(id);
     }
 
-    @PostMapping("clientes")
+    @PostMapping("/clientes")
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente create(@RequestBody Cliente cliente){
         return clienteService.save(cliente);
     }
 
+    @PutMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente update(@RequestBody Cliente cliente,@PathVariable Long id){
+        Cliente clienteActual = clienteService.findById(id);
+        clienteActual.setApellido(cliente.getApellido());
+        clienteActual.setNombre(cliente.getNombre());
+        clienteActual.setEmail(cliente.getEmail());
+        return clienteService.save(clienteActual);
+    }
+
+    @DeleteMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        clienteService.delete(id);
+    }
 
 }
